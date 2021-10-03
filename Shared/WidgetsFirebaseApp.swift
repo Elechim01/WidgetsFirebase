437d36506f895dfe71fd414e065d227584e55ad7
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-03 11:35:12 +0200

    Initial Commit

---
+++ Shared/WidgetsFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsFirebaseApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 03/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
